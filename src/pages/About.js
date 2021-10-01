44d228b098ec8cece71457e2678cf29d5ca9a3b4
sanjeevyg <yogi.sjv@gmail.com>
2021-10-01 18:44:14 -0400

    about.js updated

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,8 +4,12 @@ export default function About() {
     return (
         <div>
             <h1>Ganjo</h1>
-            <h1>Ganjo</h1>
-            <h1>Ganjo</h1>
         </div>
     )
 }
+
+    
+    
+
+
+
